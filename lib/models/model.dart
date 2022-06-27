630111dac96a0b6b6b6d972196039f50013a90dd
NadxiruL <66035094+NadxiruL@users.noreply.github.com>
2022-06-27 22:23:01 +0800

    adjusting column and row

---
+++ lib/models/model.dart
@@ -0,0 +1,13 @@
+class Book {
+  String? title;
+  String? publisher;
+  String? author;
+  int? numPages;
+
+  Book({
+    this.title,
+    this.publisher,
+    this.author,
+    this.numPages,
+  });
+}
