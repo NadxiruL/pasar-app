0f31dd1aacd095ab62ad84f99d710d593e3e5bda
NadxiruL <66035094+NadxiruL@users.noreply.github.com>
2022-07-04 17:31:36 +0800

    order screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_details_screen.dart';
 import './providers/products.dart';
+import 'package:pasar_app/providers/orders.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,6 +26,9 @@ class MyApp extends StatelessWidget {
           //provider 4.0 dan ke atas perlu guna create // 3.0 ke bawah use builder
           value: Cart(),
         ),
+        ChangeNotifierProvider.value(
+          value: Orders(),
+        )
       ],
       child: MaterialApp(
         title: 'El Coffee',
--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart.dart' show Cart;
 import '../widgets/cart_item.dart';
+import '../providers/orders.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({Key? key}) : super(key: key);
@@ -41,7 +42,11 @@ class CartScreen extends StatelessWidget {
                   ),
                   ElevatedButton(
                     onPressed: () {
-                      print('ORDERED!');
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                        cart.items!.values.toList(),
+                        cart.totalAmount,
+                      );
+                      cart.clear();
                     },
                     child: Text(
                       'Order Now',
